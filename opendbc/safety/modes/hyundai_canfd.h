22672dd959ae84ea9e32cea5c625dd3fe5f57bb4
Derrick <mcfall.derrick@gmail.com>
2026-05-15 01:58:40 -0500

    hyundai_canfd: raise torque limits to 384Nm (max_torque=384, max_rt_delta=159, max_rate_up=6, max_rate_down=7)

--- opendbc/safety/modes/hyundai_canfd.h
+++ opendbc/safety/modes/hyundai_canfd.h
@@ -142,10 +142,10 @@ static void hyundai_canfd_rx_hook(const CANPacket_t *msg) {
 
 static bool hyundai_canfd_tx_hook(const CANPacket_t *msg) {
   const TorqueSteeringLimits HYUNDAI_CANFD_STEERING_LIMITS = {
-    .max_torque = 270,
-    .max_rt_delta = 112,
-    .max_rate_up = 2,
-    .max_rate_down = 3,
+    .max_torque = 384,
+    .max_rt_delta = 159,
+    .max_rate_up = 6,
+    .max_rate_down = 7,
     .driver_torque_allowance = 250,
     .driver_torque_multiplier = 2,
     .type = TorqueDriverLimited,
